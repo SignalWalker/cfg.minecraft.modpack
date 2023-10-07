075e44eea69452dc6f34638c586e1096d5ea89be
Ash Walker <ash@ashwalker.net>
2023-10-07 18:20:55 -0400

    ...

--- nixosModule.nix
+++ nixosModule.nix
@@ -6,100 +6,114 @@
 }:
 with builtins; let
   std = pkgs.lib;
-  league = config.services.minecraft.driftingLeague;
   ini = pkgs.formats.ini {};
-in {
-  options = with lib; {
-    services.minecraft.driftingLeague = {
-      enable = mkEnableOption "drifting league server";
-      user = mkOption {
-        type = types.str;
-        default = "driftingleague";
-      };
-      group = mkOption {
-        type = types.str;
-        default = "driftingleague";
-      };
-      dir = let
-        mkDir = root:
-          mkOption {
+  minecraft = config.services.minecraft;
+  mcServer = lib.types.submoduleWith {
+    modules = [
+      ({
+        config,
+        lib,
+        pkgs,
+        name,
+        ...
+      }: {
+        options = with lib; {
+          enable = mkEnableOption "minecraft server";
+          user = mkOption {
+            type = types.str;
+            default = name;
+          };
+          group = mkOption {
+            type = types.str;
+            default = "minecraft";
+          };
+          dir = let
+            mkDir = root:
+              mkOption {
+                type = types.str;
+                readOnly = true;
+                default = "${root}/${config.user}";
+              };
+          in {
+            state = mkDir "/var/lib";
+            configuration = mkDir "/etc";
+          };
+          environmentFile = mkOption {
             type = types.str;
             readOnly = true;
-            default = "${root}/${league.user}";
+            default = "${config.dir.configuration}/env.conf";
           };
-      in {
-        state = mkDir "/var/lib";
-        configuration = mkDir "/etc";
-      };
-      environmentFile = mkOption {
-        type = types.str;
-        readOnly = true;
-        default = "${league.dir.configuration}/env.conf";
-      };
-      java = {
-        package = mkOption {
-          type = types.package;
-          default = pkgs.temurin-bin;
-        };
-        memory = {
-          initial = mkOption {
-            type = types.str;
-            default = "1024M";
+          java = {
+            package = mkOption {
+              type = types.package;
+              default = pkgs.temurin-bin;
+            };
+            memory = {
+              initial = mkOption {
+                type = types.str;
+                default = "1024M";
+              };
+              max = mkOption {
+                type = types.str;
+                default = "4096M";
+              };
+            };
           };
-          max = mkOption {
-            type = types.str;
-            default = "4096M";
+          quilt = {
+            installer = mkOption {
+              type = types.package;
+              default = pkgs.fetchurl {
+                url = "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/0.8.2/quilt-installer-0.8.2.jar";
+                hash = "sha256-fL1QnCcomJodOAay3Z1AvdgORpl7CuYbrXIz0i2k8Ao=";
+              };
+            };
+            version = mkOption {
+              type = types.str;
+              default = pack.versions.quilt;
+            };
           };
-        };
-      };
-      quilt = {
-        installer = mkOption {
-          type = types.package;
-          default = pkgs.fetchurl {
-            url = "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/0.8.2/quilt-installer-0.8.2.jar";
-            hash = "sha256-fL1QnCcomJodOAay3Z1AvdgORpl7CuYbrXIz0i2k8Ao=";
+          minecraft = {
+            version = mkOption {
+              type = types.str;
+              default = pack.versions.minecraft;
+            };
           };
+          mods = mkOption {
+            type = types.package;
+            default = self.packages.${pkgs.system}.driftingLeague;
+          };
+          prism = {
+            instance = mkOption {
+              type = types.package;
+              default = config.mods.passthru.prism;
+            };
+          };
+          rcon = {
+            package = mkOption {
+              type = types.package;
+              default = pkgs.mcrcon;
+            };
+            port = mkOption {
+              type = types.port;
+              default = 25575;
+            };
+          };
+          port = mkOption {
+            type = types.port;
+            default = 25565;
+          };
+          openFirewall = mkEnableOption "minecraft firewall";
         };
-        version = mkOption {
-          type = types.str;
-          default = pack.versions.quilt;
-        };
+      })
+    ];
+  };
+in {
+  options = with lib; {
+    services.minecraft = {
+      servers = mkOption {
+        type = types.attrsOf mcServer;
+        default = {};
       };
-      minecraft = {
-        version = mkOption {
-          type = types.str;
-          default = pack.versions.minecraft;
-        };
-      };
-      mods = mkOption {
-        type = types.package;
-        default = self.packages.${pkgs.system}.driftingLeague;
-      };
-      prism = {
-        name = mkOption {
-          type = types.str;
-          default = "prism";
-        };
-        instance = mkOption {
-          type = types.package;
-          default = league.mods.passthru.prism;
-        };
-      };
-      rcon = {
-        package = mkOption {
-          type = types.package;
-          default = pkgs.mcrcon;
-        };
-        port = mkOption {
-          type = types.port;
-          default = 25575;
-        };
-      };
-      port = mkOption {
-        type = types.port;
-        default = 25565;
-      };
-      openFirewall = mkEnableOption "minecraft firewall";
       packwiz = {
         hostName = mkOption {
           type = types.nullOr types.str;
@@ -110,76 +124,80 @@ in {
   };
   disabledModules = [];
   imports = [];
-  config = lib.mkIf league.enable {
-    users.users.${league.user} = {
-      isSystemUser = true;
-      home = league.dir.state;
-      group = league.group;
-    };
-    users.groups.${league.group} = {};
-
-    systemd.tmpfiles.rules = [
-      "L+ ${league.dir.state}/mods - - - - ${league.mods}"
-      "f ${league.environmentFile} 0640 ${league.user} ${league.group}"
-      "z ${league.environmentFile} 0640 ${league.user} ${league.group}"
-    ];
-
-    networking.firewall = lib.mkIf league.openFirewall {
-      allowedTCPPorts = [league.port];
-      allowedUDPPorts = [league.port];
-    };
-
-    systemd.services."drifting-league-setup" = {
-      after = ["network-online.target"];
-      partOf = ["drifting-league.service"];
-      path = [league.java.package];
-      serviceConfig = {
-        StateDirectory = league.user;
-        Type = "oneshot";
-        RemainAfterExit = true;
-        User = league.user;
-        Group = league.group;
+  config = lib.mkMerge ((map (serverName: let
+      server = config.services.minecraft.servers.${serverName};
+    in {
+      users.users.${server.user} = {
+        isSystemUser = true;
+        home = server.dir.state;
+        group = server.group;
       };
-      script = ''
-        set -e
-        java -jar ${league.quilt.installer} install server "${league.minecraft.version}" "${league.quilt.version}" --install-dir="${league.dir.state}" --download-server --create-scripts
-      '';
-    };
+      users.groups.${server.group} = {};
 
-    systemd.services."drifting-league" = {
-      after = ["drifting-league-setup.service"];
-      requires = ["drifting-league-setup.service"];
-      wantedBy = ["multi-user.target"];
-      path = [league.java.package];
-      restartTriggers = [league.mods];
-      serviceConfig = {
-        EnvironmentFile = league.environmentFile;
+      systemd.tmpfiles.rules = [
+        "L+ ${server.dir.state}/mods - - - - ${server.mods}"
+        "f ${server.environmentFile} 0640 ${server.user} ${server.group}"
+        "z ${server.environmentFile} 0640 ${server.user} ${server.group}"
+      ];
 
-        Type = "simple";
-        ExecStart = "${league.java.package}/bin/java -Xms${league.java.memory.initial} -Xmx${league.java.memory.max} -jar ${league.dir.state}/quilt-server-launch.jar nogui";
-        ExecStop = "${league.rcon.package}/bin/mcrcon -H localhost -P ${toString league.rcon.port} -p \"$MINECRAFT_RCON_PASSWORD\" stop";
-        SuccessExitStatus = [0 1];
-        User = league.user;
-        Group = league.group;
-
-        StateDirectory = league.user;
-        ConfigurationDirectory = league.user;
-        WorkingDirectory = league.dir.state;
-
-        ProtectHome = true;
-        ProtectSystem = "strict";
-        PrivateDevices = true;
-        NoNewPrivileges = true;
-        PrivateTmp = true;
-        ReadWritePaths = league.dir.state;
+      networking.firewall = lib.mkIf server.openFirewall {
+        allowedTCPPorts = [server.port];
+        allowedUDPPorts = [server.port];
       };
-    };
-    services.nginx.virtualHosts = lib.mkIf (league.packwiz.hostName != null) {
-      ${league.packwiz.hostName} = {
-        root = league.mods.passthru.packwizRoot;
-        locations."=/${league.prism.name}.zip".alias = league.prism.instance;
+
+      systemd.services."minecraft-${serverName}-setup" = {
+        after = ["network-online.target"];
+        partOf = ["minecraft-${serverName}.service"];
+        path = [server.java.package];
+        serviceConfig = {
+          StateDirectory = server.user;
+          Type = "oneshot";
+          RemainAfterExit = true;
+          User = server.user;
+          Group = server.group;
+        };
+        script = ''
+          set -e
+          java -jar ${server.quilt.installer} install server "${server.minecraft.version}" "${server.quilt.version}" --install-dir="${server.dir.state}" --download-server --create-scripts
+        '';
       };
-    };
-  };
+
+      systemd.services."minecraft-${serverName}" = {
+        after = ["minecraft-${serverName}-setup.service"];
+        requires = ["minecraft-${serverName}-setup.service"];
+        wantedBy = ["multi-user.target"];
+        path = [server.java.package];
+        restartTriggers = [server.mods];
+        serviceConfig = {
+          EnvironmentFile = server.environmentFile;
+
+          Type = "simple";
+          ExecStart = "${server.java.package}/bin/java -Xms${server.java.memory.initial} -Xmx${server.java.memory.max} -jar ${server.dir.state}/quilt-server-launch.jar nogui";
+          ExecStop = "${server.rcon.package}/bin/mcrcon -H localhost -P ${toString server.rcon.port} -p \"$MINECRAFT_RCON_PASSWORD\" stop";
+          SuccessExitStatus = [0 1];
+          User = server.user;
+          Group = server.group;
+
+          StateDirectory = server.user;
+          ConfigurationDirectory = server.user;
+          WorkingDirectory = server.dir.state;
+
+          ProtectHome = true;
+          ProtectSystem = "strict";
+          PrivateDevices = true;
+          NoNewPrivileges = true;
+          PrivateTmp = true;
+          ReadWritePaths = server.dir.state;
+        };
+      };
+      services.nginx.virtualHosts = lib.mkIf (minecraft.packwiz.hostName != null) {
+        ${minecraft.packwiz.hostName} = {
+          locations."=/${serverName}.zip".alias = server.prism.instance;
+          locations."/${serverName}".root = server.mods.passthru.packwizRoot;
+        };
+      };
+    }) (attrNames config.services.minecraft.servers))
+    ++ [
+    ]);
   meta = {};
 }
