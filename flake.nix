7ace2e0054bb51af3cdade6c09e72391e1cbaadb
Ash Walker <ashurstwalker@gmail.com>
2026-01-03 13:11:19 -0500

    groundwork for separate servers

--- flake.nix
+++ flake.nix
@@ -2,95 +2,126 @@
   description = "";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    alejandra = {
-      url = "github:kamadorueda/alejandra";
-      inputs.nixpkgs.follows = "nixpkgs";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
     };
   };
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    ...
-  }:
-    with builtins; let
-      std = nixpkgs.lib;
-      systems = attrNames inputs.alejandra.packages;
-      nixpkgsFor = std.genAttrs systems (system:
-        import nixpkgs {
-          localSystem = system;
-          crossSystem = system;
-          overlays = [];
-        });
-    in {
-      formatter = std.mapAttrs (system: pkgs: pkgs.default) inputs.alejandra.packages;
-      packages =
-        std.mapAttrs (system: pkgs: {
-          default = self.packages.${system}.driftingLeague;
-          driftingLeague = let
-            index = fromTOML (readFile ./pack/index.toml);
-            pack = fromTOML (readFile ./pack/pack.toml);
-            toCurseUrl = toml: let
-              fileNum = toString toml.update.curseforge.file-id;
-              fileNs = substring 0 4 fileNum;
-              fileId = std.strings.removePrefix "0" (substring 4 3 fileNum);
-            in "https://mediafilez.forgecdn.net/files/${fileNs}/${fileId}/${std.strings.escapeURL toml.filename}";
-            fetchCurseforge = toml: let
-              fileId = std.strings.splitString "0" (toString toml.update.curseforge.file-id);
-            in {
-              name = toml.filename;
-              outputHashMode = "flat";
-              outputHashAlgo = toml.download.hash-format;
-              outputHash = toml.download.hash;
-              nativeBuildInputs = with pkgs; [packwiz];
-            };
-            fetchMod = toml:
-              pkgs.fetchurl {
-                url =
-                  toml.download.url
-                  or (toCurseUrl toml);
-                outputHashAlgo = toml.download.hash-format;
-                outputHash = toml.download.hash;
-                passthru = toml;
-              };
-            mods = foldl' (acc: file: let
-              toml = fromTOML (readFile file);
-            in
-              if toml.side == "client"
-              then acc
-              else
-                acc
-                // {
-                  ${toml.name} = fetchMod toml;
-                }) {} (std.filesystem.listFilesRecursive ./pack/mods);
-          in
-            pkgs.stdenvNoCC.mkDerivation {
-              pname = pack.name;
-              version = pack.version;
-              passthru = {
-                inherit mods index pack;
-                packwizRoot = ./pack;
-              };
-              src = ./pack;
-              installPhase = let
-                installMods = std.concatStringsSep "\n" (map (mod: "ln -sT '${mod}' \"$out/${mod.passthru.filename}\"") (attrValues mods));
-              in ''
-                runHook preInstall
+  outputs =
+    inputs@{
+      flake-parts,
+      ...
+    }:
 
-                mkdir $out
-                ${installMods}
-
-                runHook postInstall
-              '';
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      let
+        serverName = "playground";
+      in
+      {
+        imports = [ ];
+        systems = [
+          "x86_64-linux"
+        ];
+        flake = {
+          nixosModules.default = (import ./nixosModule.nix) inputs serverName;
+        };
+        perSystem =
+          {
+            self',
+            pkgs,
+            # system,
+            ...
+          }:
+          {
+            formatter = pkgs.nixfmt-rfc-style;
+            devShells = {
+              default = pkgs.mkShell {
+                packages = [ pkgs.packwiz ];
+              };
             };
-        })
-        nixpkgsFor;
-      devShells =
-        std.mapAttrs (system: pkgs: {
-          default = pkgs.mkShell {
-            packages = with pkgs; [packwiz];
+            packages = {
+              default = self'.packages.${serverName};
+              ${serverName} =
+                let
+                  inherit (builtins)
+                    fromTOML
+                    readFile
+                    substring
+                    toString
+                    foldl'
+                    attrValues
+                    ;
+                  index = fromTOML (readFile ./pack/index.toml);
+                  pack = fromTOML (readFile ./pack/pack.toml);
+                  toCurseUrl =
+                    toml:
+                    let
+                      fileNum = toString toml.update.curseforge.file-id;
+                      fileNs = substring 0 4 fileNum;
+                      fileId = lib.strings.removePrefix "0" (substring 4 3 fileNum);
+                    in
+                    "https://mediafilez.forgecdn.net/files/${fileNs}/${fileId}/${lib.strings.escapeURL toml.filename}";
+                  # fetchCurseforge =
+                  #   toml:
+                  #   let
+                  #     fileId = lib.strings.splitString "0" (toString toml.update.curseforge.file-id);
+                  #   in
+                  #   {
+                  #     name = toml.filename;
+                  #     outputHashMode = "flat";
+                  #     outputHashAlgo = toml.download.hash-format;
+                  #     outputHash = toml.download.hash;
+                  #     nativeBuildInputs = [ pkgs.packwiz ];
+                  #   };
+                  fetchMod =
+                    toml:
+                    pkgs.fetchurl {
+                      url = toml.download.url or (toCurseUrl toml);
+                      outputHashAlgo = toml.download.hash-format;
+                      outputHash = toml.download.hash;
+                      passthru = toml;
+                    };
+                  mods = foldl' (
+                    acc: file:
+                    let
+                      toml = fromTOML (readFile file);
+                    in
+                    if toml.side == "client" then
+                      acc
+                    else
+                      acc
+                      // {
+                        ${toml.name} = fetchMod toml;
+                      }
+                  ) { } (lib.filesystem.listFilesRecursive ./pack/mods);
+                in
+                pkgs.stdenvNoCC.mkDerivation {
+                  pname = pack.name;
+                  version = pack.version;
+                  passthru = {
+                    inherit mods index pack;
+                    packwizRoot = ./pack;
+                  };
+                  src = ./pack;
+                  installPhase =
+                    let
+                      installMods = lib.concatStringsSep "\n" (
+                        map (mod: "ln -sT '${mod}' \"$out/${mod.passthru.filename}\"") (attrValues mods)
+                      );
+                    in
+                    ''
+                      runHook preInstall
+
+                      mkdir $out
+                      ${installMods}
+
+                      runHook postInstall
+                    '';
+                };
+            };
           };
-        })
-        nixpkgsFor;
-      nixosModules.default = (import ./nixosModule.nix) inputs (self.packages."x86_64-linux".driftingLeague.passthru.pack);
-    };
+
+      }
+    );
 }
--- nixosModule.nix
+++ nixosModule.nix
@@ -1,208 +1,284 @@
-{self, ...}: pack: {
+{ self, ... }:
+serverName:
+{
   config,
   pkgs,
   lib,
   ...
 }:
-with builtins; let
-  std = pkgs.lib;
-  league = config.services.minecraft.driftingLeague;
-  ini = pkgs.formats.ini {};
-in {
-  options = with lib; {
-    services.minecraft.driftingLeague = {
-      enable = mkEnableOption "drifting league server";
-      user = mkOption {
-        type = types.str;
-        default = "driftingleague";
+let
+  inherit (builtins) attrValues map;
+  # ini = pkgs.formats.ini { };
+in
+{
+  options =
+    let
+      mcServer = lib.submoduleWith {
+        modules = [
+          (
+            {
+              config,
+              lib,
+              pkgs,
+              name,
+              ...
+            }:
+            {
+              options =
+                let
+                  inherit (lib) mkEnableOption mkOption types;
+                in
+                {
+                  name = mkOption {
+                    type = types.str;
+                    default = name;
+                  };
+                  enable = mkEnableOption "minecraft server";
+                  user = mkOption {
+                    type = types.str;
+                    default = config.name;
+                  };
+                  group = mkOption {
+                    type = types.str;
+                    default = config.name;
+                  };
+                  dir =
+                    let
+                      mkDir =
+                        root:
+                        mkOption {
+                          type = types.str;
+                          readOnly = true;
+                          default = "${root}/${config.user}";
+                        };
+                    in
+                    {
+                      state = mkDir "/var/lib";
+                      configuration = mkDir "/etc";
+                    };
+                  environmentFile = mkOption {
+                    type = types.str;
+                    readOnly = true;
+                    default = "${config.dir.configuration}/env.conf";
+                  };
+                  java = {
+                    package = mkOption {
+                      type = types.package;
+                      default = pkgs.temurin-bin-17;
+                    };
+                    memory = {
+                      initial = mkOption {
+                        type = types.str;
+                        default = "1024M";
+                      };
+                      max = mkOption {
+                        type = types.str;
+                        default = "4096M";
+                      };
+                    };
+                  };
+                  quilt = {
+                    installer = mkOption {
+                      type = types.package;
+                      default = pkgs.fetchurl {
+                        url = "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/0.9.2/quilt-installer-0.9.2.jar";
+                        hash = "sha256-w60+I+7oYOUYXFlOfLKA5Pq+fnZqg5RTgdmpnGSFXFs=";
+                      };
+                    };
+                    version = mkOption {
+                      type = types.str;
+                      default = config.mods.passthru.pack.versions.quilt;
+                    };
+                  };
+                  minecraft = {
+                    version = mkOption {
+                      type = types.str;
+                      default = config.mods.passthru.pack.versions.minecraft;
+                    };
+                  };
+                  mods = mkOption {
+                    type = types.package;
+                  };
+                  prism = {
+                    name = mkOption {
+                      type = types.str;
+                      default = "prism";
+                    };
+                    mmcPack = mkOption {
+                      type = types.submoduleWith { modules = [ (import ./mmcPack.nix pkgs) ]; };
+                    };
+                    instance = mkOption {
+                      type = types.package;
+                      default = pkgs.stdenvNoCC.mkDerivation {
+                        name = "${config.mods.passthru.pack.name}.zip";
+                        src = ./prism;
+                        packwiz_bootstrap = pkgs.fetchurl {
+                          url = "https://github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/packwiz-installer-bootstrap.jar";
+                          hash = "sha256-qPuyTcYEJ46X9GiOgtPZGjGLmO/AjV2/y8vKtkQ9EWw=";
+                        };
+                        env = {
+                          MMC_PACK_JSON = config.prism.mmcPack.outputFile;
+                        };
+                        builder = ./build-pack.sh;
+                        nativeBuildInputs = with pkgs; [ zip ];
+                      };
+                    };
+                  };
+                  rcon = {
+                    package = mkOption {
+                      type = types.package;
+                      default = pkgs.mcrcon;
+                    };
+                    port = mkOption {
+                      type = types.port;
+                      default = 25575;
+                    };
+                  };
+                  port = mkOption {
+                    type = types.port;
+                    default = 25565;
+                  };
+                  openFirewall = mkEnableOption "minecraft firewall";
+                  packwiz = {
+                    hostName = mkOption {
+                      type = types.nullOr types.str;
+                      default = null;
+                    };
+                  };
+                };
+
+            }
+          )
+        ];
       };
-      group = mkOption {
-        type = types.str;
-        default = "driftingleague";
-      };
-      dir = let
-        mkDir = root:
-          mkOption {
-            type = types.str;
-            readOnly = true;
-            default = "${root}/${league.user}";
-          };
-      in {
-        state = mkDir "/var/lib";
-        configuration = mkDir "/etc";
-      };
-      environmentFile = mkOption {
-        type = types.str;
-        readOnly = true;
-        default = "${league.dir.configuration}/env.conf";
-      };
-      java = {
-        package = mkOption {
-          type = types.package;
-          default = pkgs.temurin-bin-17;
+    in
+    {
+      services.minecraft = {
+        servers = lib.mkOption {
+          type = lib.types.attrsOf mcServer;
+          default = { };
         };
-        memory = {
-          initial = mkOption {
-            type = types.str;
-            default = "1024M";
-          };
-          max = mkOption {
-            type = types.str;
-            default = "4096M";
-          };
-        };
       };
-      quilt = {
-        installer = mkOption {
-          type = types.package;
-          default = pkgs.fetchurl {
-            url = "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/0.9.2/quilt-installer-0.9.2.jar";
-            hash = "sha256-w60+I+7oYOUYXFlOfLKA5Pq+fnZqg5RTgdmpnGSFXFs=";
-          };
-        };
-        version = mkOption {
-          type = types.str;
-          default = pack.versions.quilt;
-        };
-      };
-      minecraft = {
-        version = mkOption {
-          type = types.str;
-          default = pack.versions.minecraft;
-        };
-      };
-      mods = mkOption {
-        type = types.package;
-        default = self.packages.${pkgs.system}.driftingLeague;
-      };
-      prism = {
-        name = mkOption {
-          type = types.str;
-          default = "prism";
-        };
-        mmcPack = mkOption {
-          type = types.submoduleWith {modules = [(import ./mmcPack.nix pkgs)];};
-        };
-        instance = mkOption {
-          type = types.package;
-          default = pkgs.stdenvNoCC.mkDerivation {
-            name = "${pack.name}.zip";
-            src = ./prism;
-            packwiz_bootstrap = pkgs.fetchurl {
-              url = "https://github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/packwiz-installer-bootstrap.jar";
-              hash = "sha256-qPuyTcYEJ46X9GiOgtPZGjGLmO/AjV2/y8vKtkQ9EWw=";
+    };
+  config =
+    let
+    in
+    lib.mkMerge (
+      [
+        (
+          let
+            server = config.services.minecraft.servers.${serverName};
+          in
+          {
+            services.minecraft.servers.${serverName} = {
+              mods = self.packages.${pkgs.stdenv.system.hostPlatform.system}.${serverName};
+              prism.mmcPack.components = {
+                "LWJGL 3".version = "3.3.1";
+                "Minecraft".version = server.minecraft.version;
+                "Quilt Loader".version = server.quilt.version;
+              };
             };
-            env = {
-              MMC_PACK_JSON = league.prism.mmcPack.outputFile;
+
+          }
+        )
+      ]
+      ++ (map (
+        server:
+        lib.mkIf server.enable (
+          let
+            systemdServiceSetup = "minecraft-server-setup-${server.name}";
+            systemdService = "minecraft-server-${server.name}";
+          in
+          {
+            users.users.${server.user} = {
+              isSystemUser = true;
+              home = server.dir.state;
+              group = server.group;
             };
-            builder = ./build-pack.sh;
-            nativeBuildInputs = with pkgs; [zip];
-          };
-        };
-      };
-      rcon = {
-        package = mkOption {
-          type = types.package;
-          default = pkgs.mcrcon;
-        };
-        port = mkOption {
-          type = types.port;
-          default = 25575;
-        };
-      };
-      port = mkOption {
-        type = types.port;
-        default = 25565;
-      };
-      openFirewall = mkEnableOption "minecraft firewall";
-      packwiz = {
-        hostName = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-        };
-      };
-    };
-  };
-  disabledModules = [];
-  imports = [];
-  config = lib.mkIf league.enable {
-    services.minecraft.driftingLeague = {
-      prism.mmcPack.components = {
-        "LWJGL 3".version = "3.3.1";
-        "Minecraft".version = league.minecraft.version;
-        "Quilt Loader".version = league.quilt.version;
-      };
-    };
+            users.groups.${server.group} = { };
 
-    users.users.${league.user} = {
-      isSystemUser = true;
-      home = league.dir.state;
-      group = league.group;
-    };
-    users.groups.${league.group} = {};
+            systemd.tmpfiles.settings = {
+              "99-minecraft-${server.name}" = {
+                "${server.dir.state}/mods" = {
+                  "L+" = {
+                    argument = server.mods;
+                  };
+                };
+                ${server.environmentFile} = {
+                  "f" = {
+                    mode = "0640";
+                    inherit (server) user group;
+                  };
+                  "z" = {
+                    mode = "0640";
+                    inherit (server) user group;
+                  };
+                };
+              };
+            };
 
-    systemd.tmpfiles.rules = [
-      "L+ ${league.dir.state}/mods - - - - ${league.mods}"
-      "f ${league.environmentFile} 0640 ${league.user} ${league.group}"
-      "z ${league.environmentFile} 0640 ${league.user} ${league.group}"
-    ];
+            networking.firewall = lib.mkIf server.openFirewall {
+              allowedTCPPorts = [ server.port ];
+              allowedUDPPorts = [ server.port ];
+            };
 
-    networking.firewall = lib.mkIf league.openFirewall {
-      allowedTCPPorts = [league.port];
-      allowedUDPPorts = [league.port];
-    };
+            systemd.services.${systemdServiceSetup} = {
+              after = [ "network-online.target" ];
+              partOf = [ "${systemdService}.service" ];
+              path = [ server.java.package ];
+              serviceConfig = {
+                StateDirectory = server.user;
+                Type = "oneshot";
+                RemainAfterExit = true;
+                User = server.user;
+                Group = server.group;
+              };
+              script = ''
+                set -e
+                java -jar ${server.quilt.installer} install server "${server.minecraft.version}" "${server.quilt.version}" --install-dir="${server.dir.state}" --download-server --create-scripts
+              '';
+            };
 
-    systemd.services."drifting-league-setup" = {
-      after = ["network-online.target"];
-      partOf = ["drifting-league.service"];
-      path = [league.java.package];
-      serviceConfig = {
-        StateDirectory = league.user;
-        Type = "oneshot";
-        RemainAfterExit = true;
-        User = league.user;
-        Group = league.group;
-      };
-      script = ''
-        set -e
-        java -jar ${league.quilt.installer} install server "${league.minecraft.version}" "${league.quilt.version}" --install-dir="${league.dir.state}" --download-server --create-scripts
-      '';
-    };
+            systemd.services.${systemdService} = {
+              after = [ "${systemdServiceSetup}.service" ];
+              requires = [ "${systemdServiceSetup}.service" ];
+              wantedBy = [ "multi-user.target" ];
+              path = [ server.java.package ];
+              restartTriggers = [ server.mods ];
+              serviceConfig = {
+                EnvironmentFile = server.environmentFile;
 
-    systemd.services."drifting-league" = {
-      after = ["drifting-league-setup.service"];
-      requires = ["drifting-league-setup.service"];
-      wantedBy = ["multi-user.target"];
-      path = [league.java.package];
-      restartTriggers = [league.mods];
-      serviceConfig = {
-        EnvironmentFile = league.environmentFile;
+                Type = "simple";
+                ExecStart = "${server.java.package}/bin/java -Xms${server.java.memory.initial} -Xmx${server.java.memory.max} -jar ${server.dir.state}/quilt-server-launch.jar nogui";
+                ExecStop = "${server.rcon.package}/bin/mcrcon -H localhost -P ${toString server.rcon.port} -p \"$MINECRAFT_RCON_PASSWORD\" stop";
+                SuccessExitStatus = [
+                  0
+                  1
+                ];
+                User = server.user;
+                Group = server.group;
 
-        Type = "simple";
-        ExecStart = "${league.java.package}/bin/java -Xms${league.java.memory.initial} -Xmx${league.java.memory.max} -jar ${league.dir.state}/quilt-server-launch.jar nogui";
-        ExecStop = "${league.rcon.package}/bin/mcrcon -H localhost -P ${toString league.rcon.port} -p \"$MINECRAFT_RCON_PASSWORD\" stop";
-        SuccessExitStatus = [0 1];
-        User = league.user;
-        Group = league.group;
+                StateDirectory = server.user;
+                ConfigurationDirectory = server.user;
+                WorkingDirectory = server.dir.state;
 
-        StateDirectory = league.user;
-        ConfigurationDirectory = league.user;
-        WorkingDirectory = league.dir.state;
-
-        ProtectHome = true;
-        ProtectSystem = "strict";
-        PrivateDevices = true;
-        NoNewPrivileges = true;
-        PrivateTmp = true;
-        ReadWritePaths = league.dir.state;
-      };
-    };
-    services.nginx.virtualHosts = lib.mkIf (league.packwiz.hostName != null) {
-      ${league.packwiz.hostName} = {
-        root = league.mods.passthru.packwizRoot;
-        locations."=/${league.prism.name}.zip".alias = league.prism.instance;
-      };
-    };
-  };
-  meta = {};
+                ProtectHome = true;
+                ProtectSystem = "strict";
+                PrivateDevices = true;
+                NoNewPrivileges = true;
+                PrivateTmp = true;
+                ReadWritePaths = server.dir.state;
+              };
+            };
+            services.nginx.virtualHosts = lib.mkIf (server.packwiz.hostName != null) {
+              ${server.packwiz.hostName} = {
+                root = server.mods.passthru.packwizRoot;
+                locations."=/${server.prism.name}.zip".alias = server.prism.instance;
+              };
+            };
+          }
+        )
+      ) (attrValues config.services.minecraft.servers))
+    );
+  meta = { };
 }
